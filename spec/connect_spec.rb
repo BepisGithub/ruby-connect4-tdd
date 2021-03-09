af70e9f5fc4be5d3eb31f3cb3d07781cef51e529
bepis <luqdek@gmail.com>
2021-03-09 17:52:37 +0000

    added a test for the draw function

--- spec/connect_spec.rb
+++ spec/connect_spec.rb
@@ -2,8 +2,20 @@ require './spec/spec_helper'
 require './scripts'
 
 describe Board do
-  it 'creates a multidimensional array that uses strings with a space in for the empty values' do
-    board = Board.new
-    expect(board.instance_variable_get(:@map)).to eq(Array.new(7, Array.new(6, ' ')))
+  describe '#initialize' do
+    it 'creates a multidimensional array that uses strings with a space in for the empty values' do
+      board = Board.new
+      expect(board.instance_variable_get(:@map)).to eq(Array.new(7, Array.new(6, ' ')))
+    end
+  end
+  describe '#draw' do
+    it 'occupies a slot with a given symbol' do
+      board = Board.new
+      horizontal_coord = 0
+      vertical_coord = 0
+      symbol = 'x'
+      board.draw(horizontal_coord, vertical_coord, symbol)
+      expect(board.instance_variable_get(:@map)).not_to eq(Array.new(7, Array.new(6, ' ')))
+    end
   end
 end
