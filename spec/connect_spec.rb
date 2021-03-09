117f664182aa2f4a77e5bf98c8a283a2d16c7087
bepis <luqdek@gmail.com>
2021-03-09 17:45:16 +0000

    added a test for a board

--- spec/connect_spec.rb
+++ spec/connect_spec.rb
@@ -1,3 +1,9 @@
 require './spec/spec_helper'
 require './scripts'
 
+describe Board do
+  it 'creates a multi dimensional array (7 horizontal, 6 vertical) to occupy slots' do
+    board = Board.new
+    expect(board.instance_variable_get(:@map)).to eq(Array.new(7, (Array.new(6))))
+  end
+end
