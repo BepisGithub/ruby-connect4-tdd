da4491dc987a978d960f2f94de6e2fecb30612e1
bepis <luqdek@gmail.com>
2021-03-09 21:29:26 +0000

    commented out next test and added a test for an occupied slots method

--- spec/connect_spec.rb
+++ spec/connect_spec.rb
@@ -23,16 +23,24 @@ describe Board do
       expect(board.write(0, 0, 'o')).to eq('error: occupied slot')
     end
   end
-  describe '#won?' do
-    it 'checks each occupied slot. for each occupied slot it checks all adjacent positions and marks down the current one
-    in an array. then it checks the adjacent ones of each position unless it is in the array. if there are four then return true' do
+
+  describe '#occupied_slots' do
+    it 'returns an array of coordinates of each occupied slot' do
       board = Board.new
       board.write(0, 0, 'x')
-      board.write(1, 1, 'x')
-      board.write(2, 2, 'x')
-      board.write(3, 3, 'x')
-      expect(board.won?).to be_truthy
+      expect(board.occupied_slots).to eq([0, 0])
     end
-    
   end
+
+  # describe '#won?' do
+  #   it 'checks each occupied slot. for each occupied slot it checks all adjacent positions and marks down the current one
+  #   in an array. then it checks the adjacent ones of each position unless it is in the array. if there are four then return true' do
+  #     board = Board.new
+  #     board.write(0, 0, 'x')
+  #     board.write(1, 1, 'x')
+  #     board.write(2, 2, 'x')
+  #     board.write(3, 3, 'x')
+  #     expect(board.won?).to be_truthy
+  #   end
+  # end
 end
