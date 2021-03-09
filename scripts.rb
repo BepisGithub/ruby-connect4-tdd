211db0d5afbfe7e4eec1ae5cbe2369357eef8c82
bepis <luqdek@gmail.com>
2021-03-09 18:33:00 +0000

    added a writing test

--- scripts.rb
+++ scripts.rb
@@ -2,4 +2,8 @@ class Board
   def initialize
     @map = Array.new(7, Array.new(6, ' '))
   end
+
+  def write(horizontal_coord, vertical_coord, symbol)
+    return 'error' unless symbol.is_a? String
+  end
 end
